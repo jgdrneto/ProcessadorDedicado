8e2a1e30b5d12e10c34c10e9351239a07ed6b9a5
Neto Gameleira <jgdrneto@gmail.com>
2016-06-07 18:34:54 -0300

    Finalizado unidade de controle e começado bloco operacional

--- processador-dedicado/blocoOperacional.vhd
+++ processador-dedicado/blocoOperacional.vhd
@@ -29,24 +29,134 @@ ENTITY blocoOperacional IS
 	lim				 :	IN STD_LOGIC_VECTOR(11 DOWNTO 0); --Limite de tempo inicial para o usuario apertar o botao depois do led acesso			
 	
 	--Saidas
-	cont				 : OUT STD_LOGIC_VECTOR(11 DOWNTO 0); --Resultado obtido no contador
-	led_0 			 : OUT STD_LOGIC;   					    --Led que indica quando o botao b2 deve ser apertado
-	led_1				 : OUT STD_LOGIC;   					    --Led que indica quando o usuario perdeu o jogo
+	cont				 : OUT STD_LOGIC_VECTOR(11 DOWNTO 0);--Resultado obtido no contador
 	c1				 	 : OUT STD_LOGIC;   					    --Resultado da comparacao =
 	c2				 	 : OUT STD_LOGIC;   					    --Resultado de uma comparaçao <=
 	tc 				 : OUT STD_LOGIC;   					    --Valor do termino de contagem
-	ledsLevel		 : OUT STD_LOGIC_VECTOR(4 DOWNTO 0);  --Leds que mostram o level do atual do jogo
+	ledsLevel		 : OUT STD_LOGIC_VECTOR(4 DOWNTO 0); --Leds que mostram o level do atual do jogo
 	regLG_0			 : OUT STD_LOGIC;   					    --Ultimo bit a esquerda do registrador dos niveis	
-	displayMilhar   : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);  --Display de 7 segmentos que mostram a(s) unidade(s) de milhar(es) do tempo descorrido
-	displayCentena  : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);  --Display de 7 segmentos que mostram a(s) dezena(s) do tempo descorrido
-	displaydezena   : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);  --Display de 7 segmentos que mostram a(s) centenas(s) do tempo descorrido
-	displayUnidade  : OUT STD_LOGIC_VECTOR(6 DOWNTO 0)   --Display de 7 segmentos que mostram a(s) unidades(s) do tempo descorrido
+	displayMilhar   : OUT STD_LOGIC_VECTOR(6 DOWNTO 0); --Display de 7 segmentos que mostram a(s) unidade(s) de milhar(es) do tempo descorrido
+	displayCentena  : OUT STD_LOGIC_VECTOR(6 DOWNTO 0); --Display de 7 segmentos que mostram a(s) dezena(s) do tempo descorrido
+	displaydezena   : OUT STD_LOGIC_VECTOR(6 DOWNTO 0); --Display de 7 segmentos que mostram a(s) centenas(s) do tempo descorrido
+	displayUnidade  : OUT STD_LOGIC_VECTOR(6 DOWNTO 0)  --Display de 7 segmentos que mostram a(s) unidades(s) do tempo descorrido
 	);			
 
 END ENTITY;
 
 ARCHITECTURE operar OF blocoOperacional IS
-
+	
+	--Componente do contador
+	COMPONENT contador
+	--Entradas e saidas do contador
+	PORT(
+		--Entradas
+		clk				 : IN STD_LOGIC;						    --Clock usado no processador dedicado
+		loadCont		    : IN STD_LOGIC; 						    --Sinal de load para o contador
+		clearCont		 : IN STD_LOGIC; 						    --Sinal de clear para o contador
+		--Saidas
+		cont				 : OUT STD_LOGIC_VECTOR(11 DOWNTO 0)  --Resultado obtido no contador
+		);
+	END COMPONENT;
+	
+	--Entidade do registrador do tempo limite para apertar o botao depois do led acesso
+	COMPONENT registradorLim
+		--Entradas e saidas do registrador de tempo limite
+		PORT(
+		--Entradas
+		clk				 : IN STD_LOGIC;						    --Clock usado no processador dedicado
+		loadRegLim		 : IN STD_LOGIC; 						    --Sinal de load para o registrador de limite
+		clearRegLim		 : IN STD_LOGIC; 						    --Sinal de clear para o registrador de limite
+		ctrlMuxRegLim	 : IN STD_LOGIC;							 --Sinal de controle para o MUX do registrador de limite
+		--lim				 :	IN STD_LOGIC_VECTOR(11 DOWNTO 0); --Limite de tempo inicial para o usuario apertar o botao depois do led acesso	
+		
+		--Saidas
+		RegLim   		 : OUT STD_LOGIC_VECTOR(11 DOWNTO 0)  --Novo limite para o usuario apertar o botao 
+		);
+	END COMPONENT;	
+		
+	--Entidade do registrador dos leds do processador dedicado
+	COMPONENT registradorLeds
+		--Entradas e saidas do registrador dos leds
+		PORT(
+		--Entradas
+		clk				 : IN STD_LOGIC;						   --Clock usado no processador dedicado
+		loadRegLG	 	 : IN STD_LOGIC;							--Sinal de load para o resgistradores dos leds dos niveis
+		clearRegLG	 	 : IN STD_LOGIC;							--Sinal de clear para o resgistradores dos leds dos niveis
+		
+		--Saidas
+		ledsLevel		 : OUT STD_LOGIC_VECTOR(4 DOWNTO 0)  --Leds que mostram o level do atual do jogo 
+		);
+	END COMPONENT;	
+	
+	--Entidade do temporizador para o bloco operacional
+	COMPONENT temporizador
+		--Entradas e saidas do temporizador
+		PORT(
+		--Entradas
+		clk				 : IN STD_LOGIC;						    --Clock usado no processador dedicado
+		loadTemp	 	    : IN STD_LOGIC;							 --Sinal de load para o temporizador
+		clearTemp	 	 : IN STD_LOGIC;							 --Sinal de clear para o temporizador
+		
+		--Saidas
+		tc			  		 : OUT STD_LOGIC 							--Valor obtido no contador 
+		);
+	END COMPONENT;
+	
+	--Entidade do registrador de numeros aleatorios para o processador dedicado
+	COMPONENT registradorPN
+		--Entradas e saidas do registrador PN
+		PORT(
+		--Entradas
+		clk				 : IN STD_LOGIC;						     --Clock usado no processador dedicado
+		--Saidas
+		RegPN			    : OUT STD_LOGIC_VECTOR(11 DOWNTO 0)  --Valor aleatorio do registrador PN
+		);
+	END COMPONENT;
+	
+	--Sinal para contador
+	SIGNAL signal_Cont,signal_RegLim, signal_RegPN : STD_LOGIC_VECTOR(11 DOWNTO 0);
+	SIGNAL signal_clk : STD_LOGIC;
+	
 	BEGIN
+	--------------------------------------------------------------------------
+	
+	CT : contador PORT MAP(clk =>clk,
+									 loadCont =>loadCont,
+									 clearCont =>clearCont,
+									 cont =>signal_Cont
+									 );
+	
+	--------------------------------------------------------------------------
+
+	REGLIM : registradorLim PORT MAP(clk=>clk,
+												loadRegLim=>loadRegLim,
+												clearRegLim=>clearRegLim,
+												ctrlMuxRegLim=>ctrlMuxRegLim,
+												RegLim=>signal_RegLim
+												);
+   
+	--------------------------------------------------------------------------
+											
+	REGLEDS : registradorLeds PORT MAP (clk=>clk,
+												   loadRegLG=>loadRegLG,
+													clearRegLG=>loadRegLG,
+													ledsLevel=>ledsLevel
+													);								 
+	
+	--------------------------------------------------------------------------
+
+	TP : temporizador PORT MAP(clk=>clk,
+										loadTemp=>loadTemp,
+										clearTemp=>loadTemp,
+										tc=>tc
+									   );	
+										
+	--------------------------------------------------------------------------									
+
+	RegPN : registradorPN PORT MAP(clk=>clk50MHz,
+											 RegPN=>signal_RegPN
+											 );
+											 
+	--------------------------------------------------------------------------										 
 
 END ARCHITECTURE;
--- processador-dedicado/unidadeDeControle.vhd
+++ processador-dedicado/unidadeDeControle.vhd
@@ -1,4 +1,4 @@
---DESCRIÇÃO: Unidade de controle do processador dedicado de um jogo
+--DESCRIÃ‡ÃƒO: Unidade de controle do processador dedicado de um jogo
 
 --Autores: Debora Emili Costa Oliveira 
 --			  Igor Macedo Silva
@@ -17,7 +17,7 @@ ENTITY unidadeDeControle IS
 	b2 				 : IN STD_LOGIC;   					   --Botao para apertar quando acender o led
 	tc 				 : IN STD_LOGIC;   					   --Valor do termino de contagem
 	c1				 	 : IN STD_LOGIC;   					   --Resultado da comparacao =
-	c2				 	 : IN STD_LOGIC;   					   --Resultado de uma comparaçao <=
+	c2				 	 : IN STD_LOGIC;   					   --Resultado de uma comparaÃ§ao <=
 	regLG_0			 : IN STD_LOGIC;   					   --Ultimo bit a esquerda do registrador dos niveis
 	reset				 : IN STD_LOGIC;						   --Botao de reset
 	clk				 : IN STD_LOGIC;						   --Clock usado no processador dedicado
@@ -48,6 +48,106 @@ ARCHITECTURE controlar OF unidadeDeControle IS
 	TYPE estado IS (INICIAL, TEMPOALEATORIO, ACENDELED, ESPERABOTAO, ACERTOU, ESPERA, PERDEU);
 	SIGNAL ea, pe : estado;
 	
+BEGIN
+	PROCESS(b1,b2,tc,c1,c2,regLG_0,reset)
 	BEGIN
-		
+		CASE ea IS
+			WHEN 	INICIAL =>			led_0 <= '0'; led_1 <= '0';
+											loadCont <= '0'; clearCont <= '1';
+											loadRegLim <= '1'; clearRegLim <= '0';	ctrlMuxRegLim <= '1';
+											loadRegLG <= '0'; clearRegLG <= '1';
+											loadTemp <= '0'; clearTemp <= '1';
+											loadRegTemp <= '0'; clearRegTemp <='1';
+											
+											IF (b1 = '1') THEN
+												pe <= TEMPOALEATORIO;
+											ELSE
+												pe <= INICIAL;
+											END IF;
+									
+			WHEN	TEMPOALEATORIO =>	led_0 <= '0'; led_1 <= '0';
+											loadCont <= '1'; clearCont <= '0';
+											loadRegLim <= '0'; clearRegLim <= '0';	ctrlMuxRegLim <= '0';
+											loadRegLG <= '0'; clearRegLG <= '0';
+											loadTemp <= '0'; clearTemp <= '1';
+											loadRegTemp <= '0'; clearRegTemp <='1';
+											
+											IF (c1 = '1') THEN
+												pe <= ACENDELED;
+											ELSE
+												pe <= TEMPOALEATORIO;
+											END IF;
+											
+			WHEN	ACENDELED =>		led_0 <= '1'; led_1 <= '0';
+											loadCont <= '0'; clearCont <= '1';
+											loadRegLim <= '0'; clearRegLim <= '0';	ctrlMuxRegLim <= '0';
+											loadRegLG <= '0'; clearRegLG <= '0';
+											loadTemp <= '0'; clearTemp <= '0';
+											loadRegTemp <= '0'; clearRegTemp <='0';
+											
+											pe <= ESPERABOTAO;
+											
+			WHEN	ESPERABOTAO =>		led_0 <= '1'; led_1 <= '0';
+											loadCont <= '1'; clearCont <= '0';
+											loadRegLim <= '0'; clearRegLim <= '0';	ctrlMuxRegLim <= '0';
+											loadRegLG <= '0'; clearRegLG <= '0';
+											loadTemp <= '0'; clearTemp <= '0';
+											loadRegTemp <= '1'; clearRegTemp <='0';
+											
+											IF (b2 = '1' AND c2 = '0') THEN
+												pe <= ACERTOU;
+											ELSIF (b2 = '0' AND c2 = '0') THEN
+												pe <= ESPERABOTAO;
+											ELSE
+												pe <= PERDEU;
+											END IF;
+											
+			WHEN	ACERTOU =>			led_0 <= '0'; led_1 <= '0';
+											loadCont <= '0'; clearCont <= '0';
+											loadRegLim <= '1'; clearRegLim <= '0';	ctrlMuxRegLim <= '0';
+											loadRegLG <= '1'; clearRegLG <= '0';
+											loadTemp <= '0'; clearTemp <= '1';
+											loadRegTemp <= '1'; clearRegTemp <='0';
+											
+											pe <= ESPERA;
+											
+			WHEN 	ESPERA =>			led_0 <= '0'; led_1 <= '0';
+											loadCont <= '0'; clearCont <= '0';
+											loadRegLim <= '0'; clearRegLim <= '0';	ctrlMuxRegLim <= '0';
+											loadRegLG <= '0'; clearRegLG <= '0';
+											loadTemp <= '1'; clearTemp <= '0';
+											loadRegTemp <= '0'; clearRegTemp <='0';
+											
+											IF (regLG_0 = '1' AND tc = '1') THEN
+												pe <= INICIAL;
+											ELSIF (regLG_0 = '0' AND tc = '1') THEN
+												pe <= TEMPOALEATORIO;
+											ELSE
+												pe <= ESPERA;
+											END IF;
+											
+			WHEN	PERDEU =>			led_0 <= '0'; led_1 <= '1';
+											loadCont <= '0'; clearCont <= '0';
+											loadRegLim <= '0'; clearRegLim <= '0';	ctrlMuxRegLim <= '0';
+											loadRegLG <= '0'; clearRegLG <= '0';
+											loadTemp <= '0'; clearTemp <= '0';
+											loadRegTemp <= '0'; clearRegTemp <='0';
+											
+											IF (reset = '1') THEN
+												pe <= INICIAL;
+											ELSE
+												pe <= PERDEU;
+											END IF;
+		END CASE;
+	END PROCESS;
+	
+	PROCESS(clk)
+	BEGIN
+		IF RISING_EDGE(clk) THEN
+			ea <= pe;
+		END IF;
+	END PROCESS;
+
+	clock50MHz <= clk50MHz;
+	
 END ARCHITECTURE;
